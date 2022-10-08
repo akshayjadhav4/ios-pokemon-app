ba44d8d7be75a479984a31076f6bf85607d5f71a
Akshay Jadhav <akshayjadhav461999@gmail.com>
2022-10-08 15:18:53 +0530

    Initial Commit

---
+++ pokemonAPI/pokemonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonAPIApp.swift
+//  pokemonAPI
+//
+//  Created by Akshay Anil Jadhav on 08/10/22.
+//
+
+import SwiftUI
+
+@main
+struct pokemonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
